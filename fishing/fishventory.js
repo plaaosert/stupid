bde9a901e4124234ec8056ac528cab9fc1f1550e
Refticus <reftiklunk@gmail.com>
2025-01-20 17:32:33 +0000

    Fishventory - See your fish again.

--- fishing/fishventory.js
+++ fishing/fishventory.js
@@ -1,9 +1,7 @@
-//builders
-const inventory_div = document.getElementById("inventory_div");
-const div_creator = document.createElement("div");
-const img_creator = document.createElement("img");
+var snd_kaching = new Audio("sound/kaching.ogg");
+var snd_click = new Audio("sound/click.ogg");
 
-let fishs = [];
+var fishs = [];
 if (localStorage.getItem("fishs") === null) {
 	localStorage.setItem("fishs", "[]");
 } else {
@@ -14,6 +12,51 @@ if (localStorage.getItem("fishs") === null) {
 	}
 }
 
-function add_inventory_item(fish) {
+function add_inventory_item(index) {
+	const inventory = document.getElementById("inventoryLimiter");
+	let rarity = fishs[index].rarity[1];
+	let sprite = fishs[index].sprite;
+	let name = fishs[index].name;
+	let value = fishs[index].value;
+	let mod_class_name = "";
+
+	if (rarity != "") {
+		if (rarity == "Pure gold") {
+			mod_class_name = "Puregold";
+		} else if (rarity == "Pure platinum") {
+			mod_class_name = "Pureplatinum";
+		} else {
+			mod_class_name = rarity;
+		}
+	} else {
+		mod_class_name = "Normal";
+		rarity = "Normal";
+	}
 	
+	const fishy_template = document.querySelector(".fishy");
+	let node = fishy_template.cloneNode(true);
+	//image
+	node.querySelector(".fishyimage").src = sprite;
+	//aboutsection
+	let aboutfish = node.querySelector(".aboutfish");
+	aboutfish.querySelector("b").className = mod_class_name;
+	aboutfish.querySelector("b").textContent = rarity;
+	aboutfish.querySelector("span.fishname").textContent = name;
+	//value
+	let fishvaluebox = node.querySelector(".fishvaluebox");
+	fishvaluebox.querySelector("span.valuetext").textContent = "Coin Value";
+	fishvaluebox.querySelector(".fishvalue").textContent = value;
+
+	inventory.appendChild(node);
+}
+
+function populate() {
+	if (fishs.length) {
+		for (let i = 0; i < fishs.length; i++) {
+			add_inventory_item(i);
+		}
+		document.getElementById("count_of_fish").innerHTML = fishs.length;
+	} else {
+		document.getElementById("inventoryLimiter").innerHTML = "you have no fish :(";
+	}
 }
