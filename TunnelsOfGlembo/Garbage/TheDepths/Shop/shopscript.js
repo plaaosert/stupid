481dea31c25fc1d137866453552f9fa62bab189b
Refticus <reftiklunk@gmail.com>
2025-01-29 21:27:25 +0000

    Nonfunctional shop commit (i'm mad)

--- TunnelsOfGlembo/Garbage/TheDepths/Shop/shopscript.js
+++ TunnelsOfGlembo/Garbage/TheDepths/Shop/shopscript.js
@@ -1,5 +1,6 @@
 var snd_pop = new Audio("pop.ogg");
 
+//if we somehow don't have them already.
 if (localStorage.getItem("coins") === null) {
 	localStorage.setItem("coins", 0);
 }
@@ -18,7 +19,7 @@ const shopkeep_lines_intro = [
 	"you've found... me.",
 	"...mhm.",
 	"you're from... glembo?",
-	"i don't sell... bait here.",
+	"...have you been fishing?",
 	"lamp... oil?",
 	"bombs...?"
 ];
@@ -29,7 +30,8 @@ const shopkeep_lines_buy = [
 	"...!",
 	"...? you wanted that? thanks...",
 	"an excellent... choice.",
-	"it's yours my... friend."
+	"it's yours my... friend.",
+	"no refunds... store policy."
 ];
 const shopkeep_lines_cant_afford = [
 	"...bring more.",
@@ -42,6 +44,8 @@ const shopkeep_lines_cant_afford = [
 
 const introtext = document.getElementById("introtext");
 
+//todo - make it pause a little on periods.
+//it's a small detail but it'll add so much.
 function write_to_div(given_text, target, clear=true) {
 	if (!given_text || !target)  {
 		console.log("Attempted to write without text or a target!");
@@ -113,14 +117,45 @@ function add_shop_item(item) {
 	textcol.querySelector(".value").textContent = price + " " + currency;
 	textcol.querySelector(".title").textContent = name;
 	textcol.querySelector(".owned").textContent = "0 owned."; //todo - include count of amount earned
+	textcol.querySelector(".owned").setAttribute("id", "item_" + id);
 	//description
 	node.querySelector(".itemdescription").textContent = description;
 	
 	wrapper.appendChild(node);
 }
+
+function currency_retriever(currency) {
+	switch(currency) {
+		case "coins":
+			return localStorage.getItem("coins");
+		case "bait":
+			return localStorage.getItem("fishing_bait");
+		default:
+			console.log("Invalid currency.");
+			return
+	}
+}
+
 //todo - this function.
 function buy_item(item_id) {
-	return;
+	const inventory = JSON.parse(localStorage.getItem("shop_items"));
+	let used_currency = currency_retriever(items[item_id][2]);
+	//you're broke.
+	if (used_currency < items[item_id][3]) {
+		write_to_div(shopkeep_lines_cant_afford[Math.floor(Math.random() * shopkeep_lines_cant_afford.length)], introtext);
+		return
+	}
+	//do this if we're just adding a value.
+	//todo - fix this because it's bork.
+	if (inventory.includes(items[item_id][1])) {
+		let item_index = inventory.indexOf(items[item_id][1]);
+		
+	} else { //if we're adding a new item to the list.
+		inventory.push([items[item_id][1], 1]);
+		localStorage.setItem("shop_items", JSON.stringify(inventory));
+	}
+	write_to_div(shopkeep_lines_buy[Math.floor(Math.random() * shopkeep_lines_buy.length)], introtext);
+	return
 }
 
 function populate_shop() {
