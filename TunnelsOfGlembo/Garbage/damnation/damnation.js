aa66c48514e54442306a43537cbfc668c06a51b5
Refticus <reftiklunk@gmail.com>
2025-02-09 14:54:46 +0000

    Embed card test. You can now also cause a Pukeko singularity.

--- TunnelsOfGlembo/Garbage/damnation/damnation.js
+++ TunnelsOfGlembo/Garbage/damnation/damnation.js
@@ -45,6 +45,11 @@ var pukekos = [];
 var purge_ready = false;
 
 function new_pukeko(x_override = null, y_override = null, no_sound = false, type_override = null) {
+	if (pukekos.length > 1000) {
+		console.log("PUKEKO SINGULARITY ACHIEVED. ANNIHILATION IMMINENT");
+		purge_pukekos();
+		return
+	}
 	let screenWidth = window.innerWidth + 40;
 	let screenHeight = window.innerHeight + 20;
 	
@@ -80,7 +85,7 @@ function new_pukeko(x_override = null, y_override = null, no_sound = false, type
 }
 
 function debug_pukeko(amt) {
-	if (amt > 2500 || pukekos.length + amt > 2500) {
+	if (amt > 1000 || pukekos.length + amt > 1000) {
 		console.log("TOO MUCH!!!!!!!!!!! (greater than 2500)");
 		return
 	}
